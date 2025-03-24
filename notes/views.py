32fabb83908d72e2342b09b0928a8c1d7f22c653
insssomniac <insssomniac@gmail.com>
2025-03-24 17:14:41 +0200

    changed views to class-based

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from .models import Notes
+
+def list(request):
+    notes = Notes.objects.all()
+    return render(request, 'notes/list.html', {'notes': notes})
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404('Note does not exist')
+    return render(request, 'notes/detail.html', {'note': note})
