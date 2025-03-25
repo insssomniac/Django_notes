db49881f0a1094a7890c8681b6ea1a13e50c32e2
insssomniac <insssomniac@gmail.com>
2025-03-25 11:52:48 +0200

    changed views to class views

--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 from .models import Notes
 
-def list(request):
-    notes = Notes.objects.all()
-    return render(request, 'notes/list.html', {'notes': notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/list.html'
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404('Note does not exist')
-    return render(request, 'notes/detail.html', {'note': note})
+class PopularNotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/list.html'
+    queryset = Notes.objects.filter(likes__gt=0).order_by('-likes')
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+    template_name = 'notes/detail.html'
