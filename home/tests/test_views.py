53043daa9e0d1dfed75def6eeb3ef2c83ec44066
insssomniac <insssomniac@gmail.com>
2025-04-06 14:04:02 +0300

    use factories in the tests

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 
-from django.contrib.auth.models import User
+from notes.tests.factories import UserFactory
 
 def test_home_endpoint_returns_welcome_page(client):
     response = client.get('/')
@@ -14,8 +14,8 @@ def test_signup_endpoint_returns_form_for_unauthenticated_user(client):
 
 @pytest.mark.django_db
 def test_signup_endpoint_redirects_authenticated_user(client):
-    user = User.objects.create_user(username='testuser', email='test@email.com', password='password')
-    client.login(username='testuser', password='password')
+    user = UserFactory.create()
+    client.login(username=user.username, password='password')
     response = client.get('/register/', follow=True)
     assert  200 == response.status_code
     assert 'notes/notes_list.html' in response.template_name
--- notes/tests/test_notes_views.py
+++ notes/tests/test_notes_views.py
@@ -1,37 +1,37 @@
 import pytest
 
-from django.contrib.auth.models import User
-from notes.models import Notes
+from notes.tests.factories import UserFactory, NoteFactory
+
+@pytest.fixture
+def logged_user(client):
+    user = UserFactory()
+    client.login(username=user.username, password='password')
+    return user
 
 @pytest.mark.django_db
-def test_list_endpoint_return_user_notes(client):
-    user = User.objects.create_user(username='testuser', email='test@email.com', password='password')
-    client.login(username='testuser', password='password')
-
-    Notes.objects.create(title='Test note', text='Test text', user=user)
-    Notes.objects.create(title='Test note 2', text='Test text 2', user=user)
+def test_list_endpoint_return_user_notes(client, logged_user):
+    note1 = NoteFactory(user=logged_user)
+    note2 = NoteFactory(user=logged_user)
 
     response = client.get('/smart/notes/')
     assert 200 == response.status_code
     content = str(response.content)
-    assert 'Test note' in content
-    assert 'Test text' in content
-    assert 'Test note 2' in content
-    assert 'Test text 2' in content
+    assert note1.title in content
+    assert note2.title in content
     assert 2 == content.count('<h3>')
 
 @pytest.mark.django_db
-def test_list_endpoint_only_list_notes_from_authenticated_user(client):
-    john = User.objects.create_user(username='john', email='john@email.com', password='password')
-    Notes.objects.create(title="Johns note", text='Test text', user=john)
+def test_list_endpoint_only_list_notes_from_authenticated_user(client, logged_user):
+    note1 = NoteFactory()
 
-    clara = User.objects.create_user(username='clara', email='clara@email.com', password='password')
-    Notes.objects.create(title="Claras note", text='Test text 2', user=clara)
+    user2 = UserFactory.create()
+    client.login(username=user2.username, password='password')
 
-    client.login(username='clara', password='password')
+    note2 = NoteFactory(user=user2)
 
     response = client.get('/smart/notes/')
     assert 200 == response.status_code
     content = str(response.content)
-    assert "Johns note" not in content
-    assert "Claras note" in content
+
+    assert note1.title not in content
+    assert note2.title in content
